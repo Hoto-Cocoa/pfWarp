c632075dfe938f857b9fa8a76b3e30f9c9497cbc
Hoto Cocoa <cocoa@hoto.us>
2022-09-15 15:11:27 +0900

    feature: ASN 기능 추가

--- index.ts
+++ index.ts
@@ -1,64 +1,24 @@
 import _fetch, { Body, RequestInit } from 'node-fetch';
 import https from 'https';
-import { AliasListResponse, AliasResponse } from './pfsense-api';
+import dotenv from 'dotenv';
+import PfsenseApi from './types/pfsense-api';
+import BgpviewApi from './types/bgpview-api';
 
-require('dotenv').config();
+dotenv.config();
 
 const agent = new https.Agent({
   rejectUnauthorized: false,
 });
 
 (async function() {
-  const aliasList = await fetch<AliasListResponse>(`${process.env.API_URL}/api/v1/firewall/alias`);
-  let cloudflare = aliasList.data.find(a => a.name === 'CLOUDFLARE');
-  if(!cloudflare) {
-    const response = await fetch<AliasResponse>(`${process.env.API_URL}/api/v1/firewall/alias`, {
-      method: 'POST',
-      body: JSON.stringify({
-        apply: true,
-        address: [],
-        name: 'CLOUDFLARE',
-        type: 'network',
-      }),
-    });
-    cloudflare = response.data;
-  }
-
-  const existingNetworks = cloudflare.address.split(' ');
-  const newNetworks = await Promise.all([
+  await createOrUpdateAlias('CLOUDFLARE', await Promise.all([
     getCloudflareNetworks('4'),
     getCloudflareNetworks('6'),
-  ]).then(([v4, v6]) => [...v4, ...v6]);
+  ]).then(([v4, v6]) => [...v4, ...v6]));
 
-  const toAdd = newNetworks.filter(n => !existingNetworks.includes(n));
-  const toRemove = existingNetworks.filter(n => !newNetworks.includes(n));
-
-  console.log('Adding', toAdd);
-  console.log('Removing', toRemove);
-
-  if(toRemove.length > 0) {
-    for(const network of toRemove) {
-      await fetch(`${process.env.API_URL}/api/v1/firewall/alias/entry`, {
-        method: 'DELETE',
-        body: JSON.stringify({
-          apply: true,
-          name: 'CLOUDFLARE',
-          address: network,
-        }),
-      });
-    }
-  }
-
-  if(toAdd.length > 0) {
-    await fetch(`${process.env.API_URL}/api/v1/firewall/alias/entry`, {
-      method: 'POST',
-      body: JSON.stringify({
-        apply: true,
-        address: toAdd,
-        name: 'CLOUDFLARE'
-      }),
-    });
-  }
+  const ASN_LIST = process.env.ASN_LIST.split(' ').map(e => e.trim()).filter(e => e.length > 0);
+  const asnNetworks = (await Promise.all(ASN_LIST.map(asn => fetch<BgpviewApi.AsnPrefixListResponse>(`https://api.bgpview.io/asn/${asn}/prefixes`, {}, 'json')))).flatMap(r => [...r.data.ipv4_prefixes, ...r.data.ipv6_prefixes]).map(p => p.prefix);
+  await createOrUpdateAlias('WARP_ASN', asnNetworks);
 })();
 
 async function fetch<T = any>(url: string, options: RequestInit = {}, contentType: KeyOfType<Body, Function> = 'json'): Promise<T> {
@@ -74,4 +34,53 @@ async function fetch<T = any>(url: string, options: RequestInit = {}, contentTyp
 async function getCloudflareNetworks(type: '4' | '6'): Promise<string[]> {
   const response = await fetch<string>(`https://www.cloudflare.com/ips-v${type}`, {}, 'text');
   return response.split('\n');
+}
+
+async function createOrUpdateAlias(name: string, newNetworks: string[]): Promise<void> {
+  const aliasList = await fetch<PfsenseApi.AliasListResponse>(`${process.env.API_URL}/api/v1/firewall/alias`);
+  let alias = aliasList.data.find(a => a.name === name);
+  if(!alias) {
+    const response = await fetch<PfsenseApi.AliasResponse>(`${process.env.API_URL}/api/v1/firewall/alias`, {
+      method: 'POST',
+      body: JSON.stringify({
+        apply: true,
+        address: [],
+        name,
+        type: 'network',
+      }),
+    });
+    alias = response.data;
+  }
+
+  const existingNetworks = alias.address.split(' ');
+
+  const toAdd = newNetworks.filter(n => !existingNetworks.includes(n));
+  const toRemove = existingNetworks.filter(n => !newNetworks.includes(n));
+
+  console.log(`Adding to ${name}:`, toAdd);
+  console.log(`Removing from ${name}:`, toRemove);
+
+  if(toRemove.length > 0) {
+    for(const network of toRemove) {
+      await fetch(`${process.env.API_URL}/api/v1/firewall/alias/entry`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+          apply: true,
+          name,
+          address: network,
+        }),
+      });
+    }
+  }
+
+  if(toAdd.length > 0) {
+    await fetch(`${process.env.API_URL}/api/v1/firewall/alias/entry`, {
+      method: 'POST',
+      body: JSON.stringify({
+        apply: true,
+        name,
+        address: toAdd,
+      }),
+    });
+  }
 }
