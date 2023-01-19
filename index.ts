02605206264bd4dab3e69eb6b4254be3c8c26ae6
Hoto Cocoa <cocoa@hoto.us>
2023-01-19 23:24:28 +0900

    feat: add delay to api fetch

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ const agent = new https.Agent({
 
   const ASN_LIST = process.env.ASN_LIST.split(',').map(e => e.trim()).filter(e => e.length).map(e => e.split(' ')).map(e => e.map(e => e.trim()));
   for(const [name, ...asn] of ASN_LIST) {
-    const asnNetworks = (await Promise.all(asn.map(Number).filter(e => !isNaN(e)).map(asn => fetch<BgpviewApi.AsnPrefixListResponse>(`https://api.bgpview.io/asn/${asn}/prefixes`, {}, 'json')))).flatMap(r => [...r.data.ipv4_prefixes, ...r.data.ipv6_prefixes]).map(p => p.prefix).filter(e => e.length);
+    const asnNetworks = (await Promise.all(asn.map(Number).filter(e => !isNaN(e)).map((asn, index) => new Promise<BgpviewApi.AsnPrefixListResponse>(resolve => setTimeout(() => resolve(fetch<BgpviewApi.AsnPrefixListResponse>(`https://api.bgpview.io/asn/${asn}/prefixes`, {}, 'json')), index * 500))))).flatMap(r => [...r.data.ipv4_prefixes, ...r.data.ipv6_prefixes]).map(p => p.prefix).filter(e => e.length);
     await createOrUpdateAlias(name, asnNetworks);
   }
 })();
